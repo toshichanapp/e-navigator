ba864cd1e077e1ee1ae9496a5885c9bd564cca93
toshichanapp <toshichan@synapz.jp>
2018-05-30 07:44:20 +0900

    validationにひっかかった時，レイアウトが崩れるのを防止する

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,14 @@ module ENavigator
       g.javascripts false
       g.helper false
     end
+
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      if instance.kind_of?(ActionView::Helpers::Tags::Label)
+        # skip when label
+        html_tag.html_safe
+      else
+        "<div class=\"has-error\">#{html_tag}<span class=\"help-block\">#{instance.error_message.first}</span></div>".html_safe
+      end
+    end
   end
 end
