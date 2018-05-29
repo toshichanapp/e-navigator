5accf954d203b9f51a159b15303ca23c93b1f1fb
toshichanapp <toshichan@synapz.jp>
2018-05-30 07:43:36 +0900

    validation追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
+
+  enum gender: {male: 0, female: 1, other: 2}
+
+  validates :name, presence: true
+  validates :birthday, presence: true
+  validates :gender, presence: true
+  validates :school_name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  enum gender: {male: 0, female: 1, other: 2}
 end
